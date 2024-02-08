eef10bbdf7ed8e5c1416cb6382ea87cc770e5950
deepakarichwal <deepakarichwal03@gmail.com>
2024-02-08 18:16:00 +0530

    Fixed icon bug in darkmode button

--- src/components/DarkModeSwitch.jsx
+++ src/components/DarkModeSwitch.jsx
@@ -1,20 +1,27 @@
 "use client";
 
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 import { HiMoon, HiSun } from "react-icons/hi2";
 
 export default function DarkModeSwitch() {
   const { theme, setTheme, systemTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
   const currentTheme = theme === "system" ? systemTheme : theme;
   const style = "text-xl hover:text-amber-500 cursor-pointer";
 
+  useEffect(function () {
+    setMounted(true);
+  }, []);
+
   return (
     <>
-      {currentTheme === "dark" ? (
-        <HiSun className={style} onClick={() => setTheme("light")} />
-      ) : (
-        <HiMoon className={style} onClick={() => setTheme("dark")} />
-      )}
+      {mounted &&
+        (currentTheme === "dark" ? (
+          <HiSun className={style} onClick={() => setTheme("light")} />
+        ) : (
+          <HiMoon className={style} onClick={() => setTheme("dark")} />
+        ))}
     </>
   );
 }
