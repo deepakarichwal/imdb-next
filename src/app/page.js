6426f05026bdc5203c9ee93d0a5b1c0bc4920fbe
deepakarichwal <deepakarichwal03@gmail.com>
2024-02-09 16:26:41 +0530

    Added Loading Effect

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,6 @@
 import MoviesList from "@/components/MoviesList";
+import next from "next";
+import { revalidatePath } from "next/cache";
 
 const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
 
@@ -12,8 +14,20 @@ export default async function Home({ searchParams }) {
       ? "/movie/top_rated"
       : "/trending/all/week";
 
+  // FOR LOADING TESTING PURPOSE
+  // const res = await new Promise((resolve) => {
+  //   setTimeout(async () => {
+  //     const response = await fetch(
+  //       `https://api.themoviedb.org/3${modifiedGenre}?api_key=${MOVIE_API_KEY}&language=en-US&page=1`,
+  //       { next: { revalidate: 60 * 60 } }
+  //     );
+  //     resolve(response);
+  //   }, 2000);
+  // });
+
   const res = await fetch(
-    `https://api.themoviedb.org/3${modifiedGenre}?api_key=${MOVIE_API_KEY}&language=en-US&page=1`
+    `https://api.themoviedb.org/3${modifiedGenre}?api_key=${MOVIE_API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 60 * 60 } }
   );
 
   if (!res.ok) {
