c9b943e7dd5d6f35cef0eccb5aefd6020ccd6382
deepakarichwal <deepakarichwal03@gmail.com>
2024-02-10 13:26:40 +0530

    Fixing movie page

--- src/app/Providers.jsx
+++ src/app/Providers.jsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from "next-themes";
 export default function Providers({ children }) {
   return (
     <ThemeProvider defaultTheme="system" attribute="class">
-      <div className="text-gray-800 dark:text-gray-100 dark:bg-gray-900 min-h-screen select-none transition-colors duration-200">
+      <div className="text-gray-800 dark:text-gray-100 dark:bg-gray-900 min-h-screen select-none transition-colors duration-200 box-border">
         {children}
       </div>
     </ThemeProvider>
--- src/app/search/[searchTerm]/page.jsx
+++ src/app/search/[searchTerm]/page.jsx
@@ -1,20 +1,25 @@
-import MovieCard from "@/components/MovieCard";
+import MoviesList from "@/components/MoviesList";
 import React from "react";
 
 const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
 
 export default async function Search({ params }) {
   const res = await fetch(
-    `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${params.searchTerm}`
+    `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${params.searchTerm}&language=en-US&page=1&include_adult=false`
   );
 
-  const movies = await res.json();
+  const data = await res.json();
+  const result = data.results;
 
   return (
-    <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-6 max-w-6xl mx-auto py-16 px-4">
-      {movies.results.map((movie) => (
-        <MovieCard key={movie.id} movie={movie} />
-      ))}
+    <div>
+      {result && result.length === 0 ? (
+        <h1 className="text-center mt-6">
+          No result found with the "{params.searchTerm}" keyword
+        </h1>
+      ) : (
+        result && <MoviesList movies={result} />
+      )}
     </div>
   );
 }
